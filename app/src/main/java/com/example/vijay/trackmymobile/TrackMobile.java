7fac6dcce0be0573c54b2e647948d5dc7d470798
omkarvijay6 <omkarvijay6@gmail.com>
2016-01-17 17:13:50 +0530

    live tracking logic

--- app/src/main/java/com/example/vijay/trackmymobile/TrackMobile.java
+++ app/src/main/java/com/example/vijay/trackmymobile/TrackMobile.java
@@ -1,22 +1,58 @@
 package com.example.vijay.trackmymobile;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class TrackMobile extends FragmentActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class TrackMobile extends FragmentActivity implements
+        LocationListener,
+        ConnectionCallbacks,
+        OnConnectionFailedListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private static final String TAG = "TrackMobileActivity";
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest = new LocationRequest();
+    private Location mCurrentLocation;
+    private LatLng latlong;
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    String mLastUpdateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_mobile);
         setUpMapIfNeeded();
+
+        createLocationRequest();
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     @Override
@@ -48,7 +84,8 @@ public class TrackMobile extends FragmentActivity {
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                setUpMap();
+                latlong = new LatLng(49, 79);
+                setUpMap(latlong);
             }
         }
     }
@@ -59,7 +96,99 @@ public class TrackMobile extends FragmentActivity {
      * <p>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    private void setUpMap(LatLng latlong) {
+        mMap.addMarker(new MarkerOptions().position(latlong).title("Marker"));
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        Log.d(TAG, "Connection suspended ...............");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        startLocationUpdates();
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed: " + connectionResult.toString());
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        Log.d(TAG, "Location update stopped .......................");
+    }
+
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        if (mGoogleApiClient.isConnected()) {
+//            startLocationUpdates();
+//            Log.d(TAG, "Location update resumed .....................");
+//        }
+//    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+        Log.d(TAG, "Location update onStart .....................");
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+        Log.d(TAG, "Location update onStop .....................");
+    }
+
+    private void updateUI() {
+        if (null != mCurrentLocation){
+            String lat = String.valueOf(mCurrentLocation.getLatitude());
+            String lng = String.valueOf(mCurrentLocation.getLongitude());
+
+            TextView mlocationText = new TextView(this);
+            mlocationText.setText("At Time: " + mLastUpdateTime + "\n" +
+                    "Latitude: " + lat + "\n" +
+                    "Longitude: " + lng + "\n" +
+                    "Accuracy: " + mCurrentLocation.getAccuracy() + "\n" +
+                    "Provider: " + mCurrentLocation.getProvider());
+//            RelativeLayout layout = (RelativeLayout) findViewById(R.id.location);
+//            layout.addView(mlocationText);
+        } else {
+            Log.d(TAG, "location is null ...............");
+        }
+    }
+
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 }
+
+
+
