b776233acbcdfef19954848e7b82a1d1560a3f32
omkarvijay6 <omkarvijay6@gmail.com>
2016-01-17 15:38:15 +0530

    added button onclick event

--- app/src/main/java/com/example/vijay/trackmymobile/MainActivity.java
+++ app/src/main/java/com/example/vijay/trackmymobile/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.vijay.trackmymobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void trackMyLocation(View view) {
+        Intent intent = new Intent(this, TrackMobile.class);
+        startActivity(intent);
+
     }
 }
