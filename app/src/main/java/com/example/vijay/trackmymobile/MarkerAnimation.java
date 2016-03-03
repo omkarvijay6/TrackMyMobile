db894d6d48f6119876f9b2525e827a31f04db49c
omkarvijay5 <omkarvijay5@gmail.com>
2016-03-03 16:19:15 +0530

    rotate marker with animation

--- app/src/main/java/com/example/vijay/trackmymobile/MarkerAnimation.java
+++ app/src/main/java/com/example/vijay/trackmymobile/MarkerAnimation.java
@@ -10,9 +10,11 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.util.Log;
 import android.util.Property;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -20,6 +22,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 public class MarkerAnimation {
+    private static final String TAG = "MarkerAnimation";
 
     public static void animateMarkerToGB(final Marker marker, final LatLng finalPosition,
                                           final LatLngInterpolator latLngInterpolator) {
@@ -101,9 +104,35 @@ public class MarkerAnimation {
                 dLong = (2.0 * Math.PI + dLong);
         }
         float brng = (float) ((Math.toDegrees(Math.atan2(dLong, dPhi)) + 360.0) % 360.0);
-        marker.setRotation(brng);
+//        marker.setRotation(brng);
+        rotateMarker(marker, brng);
         animator.setDuration(5000);
         animator.start();
+    }
+
+    static public void rotateMarker(final Marker marker, final float brng) {
+    final Handler handler = new Handler();
+    final long start = SystemClock.uptimeMillis();
+    final float startRotation = marker.getRotation();
+    final long duration = 355;
+
+    final Interpolator interpolator = new LinearInterpolator();
+
+    handler.post(new Runnable() {
+        @Override
+        public void run() {
+            long elapsed = SystemClock.uptimeMillis() - start;
+            float t = interpolator.getInterpolation((float) elapsed / duration);
+
+            float rot = t * brng + (1 -t) * startRotation;
+
+            marker.setRotation(-rot > 180 ? rot/2 : rot);
+            if (t < 1.0) {
+                // Post again 16ms later.
+                handler.postDelayed(this, 16);
+            }
+        }
+    });
     }
 
 }
